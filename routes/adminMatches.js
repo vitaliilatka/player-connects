f875fd77e902375b8e01754509c1814a109b01e9
vlatka <latka.vitalii@gmail.com>
2026-02-19 15:51:02 +0100

    deleted some commented part of code

--- routes/adminMatches.js
+++ routes/adminMatches.js
@@ -118,11 +118,6 @@ router.post("/:matchId/lineup", authMiddleware("admin"), async (req, res) => {
       position: p.position.toLowerCase(),
     }));
 
-    // match.subsIn[team] = subs.map(s => ({
-    //   player: new mongoose.Types.ObjectId(s.playerId),
-    //   minute: s.minute || null
-    // }));
-
     match.status = "live";
     await match.save();
 
@@ -208,57 +203,3 @@ router.post(
 
 
 export default router;
-
-/**
- * POST /admin/matches/:matchId/result
- * Фактические замены и MOTM
- */
-
-// router.post(
-//   "/:id/result",
-//   authMiddleware("admin"),
-//   async (req, res) => {
-//     try {
-//       const { team, subs = [], goals = [], motm = null, score } = req.body;
-
-//       const match = await Match.findById(req.params.id);
-//       if (!match) {
-//         return res.status(404).json({ message: "Match not found" });
-//       }
-
-//       if (!["home", "away"].includes(team))
-//         return res.status(400).json({ message: "Invalid team" });
-
-//       if (subs && team) {
-//         match.subsIn[team] = subs;
-//       }
-
-//       if (goals && team) {
-//         match.events.goals[team] = goals;
-//       }
-
-//       if (score) {
-//         match.score.home = score.home;
-//         match.score.away = score.away;
-//       }
-
-//       if (motm) {
-//         match.events.motm = motm;
-//       }
-
-//       match.status = "finished";
-
-//       await match.save();
-
-//       res.json({ message: "Match result updated" });
-
-//     } catch (err) {
-//       res.status(500).json({ message: err.message });
-//     }
-//   }
-// );
-
-
-
-// export default router;
-
